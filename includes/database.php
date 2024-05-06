3cd0f8321a9b2ca806438218e0ff38d09d6086d6
Lance <lance.auro.c@gmail.com>
2024-05-06 23:33:56 +0800

    added initial code for db connection

---
+++ includes/database.php
@@ -0,0 +1,7 @@
+<?php 
+
+// The following parameters are taken by mysqli_connect("localhost","my_user","my_password","my_db")
+$connect = mysqli_connect('localhost', 'php-sql-cms-sample', 'phpsqlcmssample', 'php-sql-cms-sample');
+
+
+?>
