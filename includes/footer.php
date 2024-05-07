54e9d431e7cd4229c01b2a05ef539f549b8924fb
Lance <lance.auro.c@gmail.com>
2024-05-07 22:25:14 +0800

    linked css and js files

--- includes/footer.php
+++ includes/footer.php
@@ -1,2 +1,4 @@
+<script type="text/javascript" src="js/mdb.umd.min.js"></script>
+
 </body> 
 </html>
--- includes/header.php
+++ includes/header.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE-edge">
     <meta name="viewport" content="width-device-width, initial-scale-1.0">
+    <title>CMS Sample</title>
     <link
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
@@ -13,6 +14,5 @@
       href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap"
     />
     <link rel="stylesheet" href="css/mdb.min.css" />
-    <title>Document</title>
 </head>
 <body>
