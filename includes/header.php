cf16ed58a6cf50f0c2f854e1715dd910c1461576
Lance <lance.auro.c@gmail.com>
2024-05-08 23:48:14 +0800

    added login ui

--- includes/header.php
+++ includes/header.php
@@ -17,7 +17,7 @@
 </head>
 <nav class="navbar navbar-expand-lg navbar-light bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">Sample CMS</a>
     <button
       data-mdb-collapse-init
       class="navbar-toggler"
@@ -35,15 +35,10 @@
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
+          <a class="nav-link" href="#">Dashboard</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled"
-            >Disabled</a
-          >
+          <a class="nav-link" href="#">Logout</a>
         </li>
       </ul>
     </div>
