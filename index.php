123b580a04f5dbf16b1ac401399cd5a70e0dd43c
Lance <lance.auro.c@gmail.com>
2024-05-09 21:28:52 +0800

    video checkpoint, end of Part  4

--- index.php
+++ index.php
@@ -14,7 +14,18 @@ if (isset($_POST['email'])) {
         $result = $stm->get_result();
         $user = $result->fetch_assoc();
 
-        var_dump($user); // Displays the result array
+        if ($user) {
+            $_SESSION['id'] = $user['id'];
+            $_SESSION['email'] = $user['email'];
+            $_SESSION['username'] = $user['username'];
+
+            // TODO :: give a feedback / welcome message
+            header('location: dashboard.php');
+            die();
+        }
+        $stm->close();
+    } else {
+        echo "Statement could not be prepared.";
     }
 }
 
@@ -32,19 +43,6 @@ if (isset($_POST['email'])) {
                 <div data-mdb-input-init class="form-outline mb-4">
                     <input type="password" id="password" name="password" class="form-control" />
                     <label class="form-label" for="password">Password</label>
-                </div>
-
-                <div class="row mb-4">
-                    <div class="col d-flex justify-content-center">
-                        <div class="form-check">
-                            <input class="form-check-input" type="checkbox" value="" id="form1Example3" checked />
-                            <label class="form-check-label" for="form1Example3"> Remember me </label>
-                        </div>
-                    </div>
-
-                    <div class="col">
-                        <a href="#!">Forgot password?</a>
-                    </div>
                 </div>
 
                 <button data-mdb-ripple-init type="submit" class="btn btn-primary btn-block">Sign in</button>
