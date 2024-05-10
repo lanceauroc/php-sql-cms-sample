e60f113be0e2f07b925f2e2f57dbd04b4d647a24
Lance <lance.auro.c@gmail.com>
2024-05-11 01:53:22 +0800

    added logout

--- includes/header.php
+++ includes/header.php
@@ -26,7 +26,7 @@
           <a class="nav-link" href="#">Dashboard</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Logout</a>
+          <a class="nav-link" href="logout.php">Logout</a>
         </li>
       </ul>
     </div>
---
+++ logout.php
@@ -0,0 +1,8 @@
+<?php
+
+include ('includes/config.php');
+
+session_destroy();
+
+header('location: /php-sql-cms-sample');
+?>
