ff4cd513b32c2031cfd596306dbe6b3feaa64f68
Lance <lance.auro.c@gmail.com>
2024-05-15 12:12:47 +0800

    video checkpoint 8 02 creating table contents

--- users.php
+++ users.php
@@ -14,7 +14,7 @@ if ($stm = $connect->prepare('SELECT * FROM users')) {
     $user = $result->fetch_assoc();
 
     var_dump($user);
-    die();
+    // die();  
     if ($user) {
 
         ?>
@@ -23,18 +23,33 @@ if ($stm = $connect->prepare('SELECT * FROM users')) {
             <div class="row justify-content-center">
                 <div class="col-md-6">
                     <h1 class="display-1">Users Management</h1>
-                    <table>
-                        <!-- TODO:: Place table elements here -->
+                    <table class="table table-striped table-hover">
+                        <tr>
+                            <th>Id</th>
+                            <th>Username</th>
+                            <th>Email</th>
+                            <th>Status</th>
+                            <th>Edit | Delete</th>
+                        </tr>
+
+                        <?php while ($record = mysqli_fetch_assoc($result)) { ?>
+                            <tr>
+                                <!-- TODO:: Records should appear here -->
+                            </tr>
+
+
+                        <?php } ?>
+
                     </table>
                 </div>
             </div>
         </div>
 
         <?php
+    } else {
+        echo "No users found.";
+        $stm->close();
     }
-
-    echo "No users found.";
-    $stm->close();
 
 } else {
     echo "Statement could not be prepared.";
