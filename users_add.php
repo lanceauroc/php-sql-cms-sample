2f30fa14e3a4d5bfc1792589ef07a20a2dd5f0b3
Lance <lance.auro.c@gmail.com>
2024-05-17 16:30:07 +0800

    added username form input

--- users_add.php
+++ users_add.php
@@ -14,7 +14,24 @@ secure();
     <div class="row justify-content-center">
         <div class="col-md-6">
             <h1 class="display-1">Add User</h1>
+            <form method="post">
+                <div data-mdb-input-init class="form-outline mb-4">
+                    <input type="text" id="username" name="username" class="form-control" />
+                    <label class="form-label" for="username">Username</label>
+                </div>
 
+                <div data-mdb-input-init class="form-outline mb-4">
+                    <input type="email" id="email" name="email" class="form-control" />
+                    <label class="form-label" for="email">Email address</label>
+                </div>
+
+                <div data-mdb-input-init class="form-outline mb-4">
+                    <input type="password" id="password" name="password" class="form-control" />
+                    <label class="form-label" for="password">Password</label>
+                </div>
+
+                <button data-mdb-ripple-init type="submit" class="btn btn-primary btn-block">Sign in</button>
+            </form>
         </div>
     </div>
 </div>
