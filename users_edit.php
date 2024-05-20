81758d78fc2637aadcbd97e1033bd5e939219a5d
Lance <lance.auro.c@gmail.com>
2024-05-20 18:27:20 +0800

    video checkpoint 26 04 edit user forms

--- users_edit.php
+++ users_edit.php
@@ -20,43 +20,63 @@ if (isset($_POST['username'])) {
     }
 }
 
-var_dump($_GET); //Displays in an array the value from the URL or from GET
-?>
+if (isset($_GET['id'])) {
+    if ($stm = $connect->prepare('SELECT * FROM users WHERE id = ?')) {
+        $stm->bind_param('i', $_GET['id']);
+        $stm->execute();
 
-<div class="container mt-5">
-    <div class="row justify-content-center">
-        <div class="col-md-6">
-            <h1 class="display-1">Edit User</h1>
-            <form method="post">
-                <div data-mdb-input-init class="form-outline mb-4">
-                    <input type="text" id="username" name="username" class="form-control" />
-                    <label class="form-label" for="username">Username</label>
+        $result = $stm->get_result();
+        $user = $result->fetch_assoc();
+
+        if ($user) {
+
+
+            ?>
+
+            <div class="container mt-5">
+                <div class="row justify-content-center">
+                    <div class="col-md-6">
+                        <h1 class="display-1">Edit User</h1>
+                        <form method="post">
+                            <div data-mdb-input-init class="form-outline mb-4">
+                                <input type="text" id="username" name="username" class="form-control"
+                                    value="<?php echo $user['username']; ?>" />
+                                <label class="form-label" for="username">Username</label>
+                            </div>
+
+                            <div data-mdb-input-init class="form-outline mb-4">
+                                <input type="email" id="email" name="email" class="form-control" />
+                                <label class="form-label" for="email">Email address</label>
+                            </div>
+
+                            <div data-mdb-input-init class="form-outline mb-4">
+                                <input type="password" id="password" name="password" class="form-control" />
+                                <label class="form-label" for="password">Password</label>
+                            </div>
+
+                            <div data-mdb-input-init class="form-outline mb-4">
+                                <select name="active" class="form-select" id="active">
+                                    <option value="1">Active</option>
+                                    <option value="0">Inactive</option>
+                                </select>
+                            </div>
+
+                            <button data-mdb-ripple-init type="submit" class="btn btn-primary btn-block">Add User</button>
+                        </form>
+                    </div>
                 </div>
+            </div>
 
-                <div data-mdb-input-init class="form-outline mb-4">
-                    <input type="email" id="email" name="email" class="form-control" />
-                    <label class="form-label" for="email">Email address</label>
-                </div>
-
-                <div data-mdb-input-init class="form-outline mb-4">
-                    <input type="password" id="password" name="password" class="form-control" />
-                    <label class="form-label" for="password">Password</label>
-                </div>
-
-                <div data-mdb-input-init class="form-outline mb-4">
-                    <select name="active" class="form-select" id="active">
-                        <option value="1">Active</option>
-                        <option value="0">Inactive</option>
-                    </select>
-                </div>
-
-                <button data-mdb-ripple-init type="submit" class="btn btn-primary btn-block">Add User</button>
-            </form>
-        </div>
-    </div>
-</div>
-
-<?php
-
+            <?php
+        }
+        $stm->close();
+        die();
+    } else {
+        echo "Statement could not be prepared.";
+    }
+} else {
+    echo "No user selected.";
+    die();
+}
 
 include ('includes/footer.php'); ?>
